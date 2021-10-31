eb395a5a0cb81e8442c7684d15c1a9aca899e641
Sebastien Pierre-Jacques <sebastienpj@protonmail.com>
2021-10-31 18:14:04 -0500

    refactored tasks in todoFactory as objects

--- src/index.js
+++ src/index.js
@@ -3,71 +3,70 @@ import './styles.css';
 
 const todo = (() => {
 
-
   const todoFactory = (data) => {
     let todoObj = {};
-    let allTasks = [];
-
-
+    let tasks = [];
 
 
     for(let pair of data.entries()) {
       let key = pair[0];
       let value = pair[1];
 
-      if (key.includes('task')) {
-        allTasks.push(value)
-      } else {
-        todoObj[key] = value;
-      };
 
-      // console.log(key[0].includes('task'));
+      if (key.includes('task')) {
+        let taskObj = {};
+
+        taskObj[key] = value;
+        taskObj['isTaskDone'] = 'no'
+        tasks.push(taskObj);
+
+        // console.log(taskObj);
+        // console.log(tasks);
+      } else {
+
+        todoObj[key] = value;
+
+      };
     };
 
-    todoObj['tasks'] = allTasks;
+
+    todoObj['all-tasks'] = tasks;
+
+    // console.log(todoObj);
+
     
     return todoObj
   };
-    
-    
-    
+
+
+
   const captureFormData = () => {
     return new FormData(_todoForm)
   };
 
-
   const getList = () => {
     return _todoList
   };
-    
-    
-    
+
+
   const startTodoApp = function(e) {
-    e.preventDefault();   
+    e.preventDefault();     
     
-    
-    let _formData = captureFormData();
-    
+    let _formData = captureFormData();    
 
     let _newTodo = todoFactory(_formData);
-    // console.log(_newTodo);
 
     _todoList.push(_newTodo);
 
 
     _todoForm.reset();
 
-
-    // renderTodo.updatePage(_todoList);
-    renderTodo.updatePage(_newTodo);
-
-
-    
+    renderTodo.updatePage(_newTodo);    
         
   };   
-    
 
-    
+
+
   const _todoForm = document.querySelector(".todoForm");
   const _submitButton = document.querySelector('.submit');
 
@@ -75,20 +74,16 @@ const todo = (() => {
 
 
   _submitButton.addEventListener('click', startTodoApp);
-   
 
   return { getList }
+
 
 })();
 
 
 export { todo }
 
-// const todoFactory = (title, desctiption, dueDate, priority, notes, checklist) => {
-//     const name = () => {console.log(this.title)};
 
-//     return { title, desctiption, dueDate, priority, notes, checklist }
-// }
 
 
 
