e01c8ba01fa6ddc208c2d94c09f60f427c8464f9
Sebastien <sebastienpj@protonmail.com>
2022-12-04 20:38:44 -0600

    update functions

--- src/render.js
+++ src/render.js
@@ -1,149 +1,11 @@
-import { todo } from "./index";
-import { createElements } from "./create";
-import { format } from "date-fns";
+/* eslint-disable no-underscore-dangle */
+import { todo } from './index';
+import { createElements } from './create';
+import { format } from 'date-fns';
 
 const renderTodo = (() => {
-
-  const updatePage = (menuButtonSelected) => {
-
-    clearDisplay();
-
-    _contentDisplay.appendChild(createElements.createDisplayHeader(menuButtonSelected))
-
-    _contentDisplay.appendChild(createElements.createPageList(menuButtonSelected))
-
-
-    _contentDisplay.appendChild(createElements.createAddButton(menuButtonSelected));
-  
-  };
-
-
-  const clearDisplay = () => {
- 
-    while (_contentDisplay.hasChildNodes()) {
- 
-      _contentDisplay.removeChild(_contentDisplay.firstChild)
-    }
-  };
-
-
-  const stopScrolling = () => {
-
-    document.classList.toggle('stop-scolling')
-  }
-
-
-  
-  const openForm = (form) => {
-    form.setAttribute('style', 'display: flex');
-    form.classList.add('current-form-inuse');
-    window.scrollTo({top:0});
-    document.documentElement.classList.toggle('stop-scrolling');
-    _app.classList.toggle('move-to-background');
-
-  };
-
-  
-
-  const closeForm = (formChosen) => {
-    formChosen.classList.remove('current-form-inuse');
-    formChosen.setAttribute('style', 'display: none');
-    document.documentElement.classList.toggle('stop-scrolling')
-    _app.classList.toggle('move-to-background');
-
-
-
-  };
-
-
-
-
-
-
-
-  const clearOldOptions = (tag) => {
-    let options = [...tag.options];
-  
-    options.forEach(option => {
-      if (option.textContent != "") {
-        option.remove()
-      }
-    })
-
-  };
-
-
-  const addItemsToFormOptions = (selectTag, array) => {
-    clearOldOptions(selectTag);
-
-    array.forEach(item => {
-      let newOption = document.createElement('option');
-      typeof item == 'object'? newOption.textContent = item.title: newOption.textContent = item;
-      selectTag.add(newOption)      
-    });
-
-  };
-
-
-
-  const prefillEditForm = (e) => { 
-
-    let _todoIndex = e.target.dataset.overallTaskIndex
-
-    let _allTodoArray = todo.getTodoList();
-
-    let _currentTodoObject = _allTodoArray[_todoIndex];
-
-
-    _editTitle.value = _currentTodoObject.title;
-    
-    if (_currentTodoObject.hasOwnProperty('notes')) {
-      _editNotes.value = _currentTodoObject.notes;
-    };
-
-    if (_currentTodoObject.hasOwnProperty('dueDate') && _currentTodoObject.dueDate !== '') {
-      _editDueDate.value = format(_currentTodoObject.dueDate, 'yyyy-MM-dd');
-    }
-
-
-    if (_currentTodoObject.hasOwnProperty('priority')) {
-      _editPriority.value = _currentTodoObject.priority;
-    }
-
-
-    if (_currentTodoObject.hasOwnProperty('project')) {
-      _editProject.value = _currentTodoObject.project;
-    }
-
-  };
-
-
-
-  const prefillProjForm = (values, optionsArray) => {
-
-    let _currentProjName = values.listOfProjects[values.projectIndex]
-
-    _editProjName.value = _currentProjName
-
-    let _selectedValues = values.tasksBelongingToProj.map(task => task.title)
-    
-    for (const option of optionsArray) {
-  
-      option.removeAttribute('selected');
-
-      if (_selectedValues.includes(option.value)) {
-        option.setAttribute('selected', 'selected');
-      }
-    
-    };
-
-  };
-
-
-
   const _contentDisplay = document.querySelector('.todo-display');
-  const _app = document.querySelector('.todo-app')
-
+  const _app = document.querySelector('.todo-app');
 
   const _editTitle = document.querySelector('#edit-title');
   const _editNotes = document.querySelector('#edit-notes');
@@ -151,15 +13,114 @@ const renderTodo = (() => {
   const _editPriority = document.querySelector('#edit-priority');
   const _editProject = document.querySelector('#edit-form-proj-select-tag');
 
-
   const _editProjName = document.querySelector('#edit-project-name');
 
+  const clearDisplay = () => {
+    while (_contentDisplay.hasChildNodes()) {
+      _contentDisplay.removeChild(_contentDisplay.firstChild);
+    }
+  };
 
+  const updatePage = (menuButtonSelected) => {
+    clearDisplay();
 
+    _contentDisplay.appendChild(createElements.createDisplayHeader(menuButtonSelected));
 
- 
-  return { updatePage, openForm, closeForm, addItemsToFormOptions, prefillEditForm, prefillProjForm, stopScrolling }
+    _contentDisplay.appendChild(createElements.createPageList(menuButtonSelected));
+
+    _contentDisplay.appendChild(createElements.createAddButton(menuButtonSelected));
+  };
+
+  const stopScrolling = () => {
+    document.classList.toggle('stop-scrolling');
+  };
+
+  const openForm = (form) => {
+    form.setAttribute('style', 'display: flex');
+    form.classList.add('current-form-inuse');
+    window.scrollTo({ top: 0 });
+    document.documentElement.classList.toggle('stop-scrolling');
+    _app.classList.toggle('move-to-background');
+  };
+
+  const closeForm = (formChosen) => {
+    formChosen.classList.remove('current-form-inuse');
+    formChosen.setAttribute('style', 'display: none');
+    document.documentElement.classList.toggle('stop-scrolling');
+    _app.classList.toggle('move-to-background');
+  };
+
+  const clearOldOptions = (tag) => {
+    const options = [...tag.options];
+
+    options.forEach((option) => {
+      if (option.textContent != '') {
+        option.remove();
+      }
+    });
+  };
+
+  const addItemsToFormOptions = (selectTag, array) => {
+    clearOldOptions(selectTag);
+
+    array.forEach((item) => {
+      const newOption = document.createElement('option');
+      typeof item == 'object' ? newOption.textContent = item.title : newOption.textContent = item;
+      selectTag.add(newOption);
+    });
+  };
+
+  const prefillEditForm = (e) => {
+    const _todoIndex = e.target.dataset.overallTaskIndex;
+
+    const _allTodoArray = todo.getTodoList();
+
+    const _currentTodoObject = _allTodoArray[_todoIndex];
+
+    _editTitle.value = _currentTodoObject.title;
+
+    if (_currentTodoObject.hasOwnProperty('notes')) {
+      _editNotes.value = _currentTodoObject.notes;
+    }
+
+    if (_currentTodoObject.hasOwnProperty('dueDate') && _currentTodoObject.dueDate !== '') {
+      _editDueDate.value = format(_currentTodoObject.dueDate, 'yyyy-MM-dd');
+    }
+
+    if (_currentTodoObject.hasOwnProperty('priority')) {
+      _editPriority.value = _currentTodoObject.priority;
+    }
+
+    if (_currentTodoObject.hasOwnProperty('project')) {
+      _editProject.value = _currentTodoObject.project;
+    }
+  };
+
+  const prefillProjForm = (values, optionsArray) => {
+    const _currentProjName = values.listOfProjects[values.projectIndex];
+
+    _editProjName.value = _currentProjName;
+
+    const _selectedValues = values.tasksBelongingToProj.map((task) => task.title);
+
+    for (const option of optionsArray) {
+      option.removeAttribute('selected');
+
+      if (_selectedValues.includes(option.value)) {
+        option.setAttribute('selected', 'selected');
+      }
+    }
+  };
+
+  return {
+    updatePage,
+    openForm,
+    closeForm,
+    addItemsToFormOptions,
+    prefillEditForm,
+    prefillProjForm,
+    stopScrolling,
+  };
 })();
 
-
-export { renderTodo }
+export { renderTodo };
